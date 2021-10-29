f71549f7e3718478ad0a4c179570b7f5b644e572
Katrin Porkul <86647070+Porkul@users.noreply.github.com>
2021-10-30 01:30:02 +0300

    Update array.js

--- homework/array.js
+++ homework/array.js
@@ -1,5 +1,4 @@
 // 1. Võta array [2,7,6,12,44,32,4,8,12,17] ja leidke seal olevate arvude kogusumma
-
 let arr = [2,7,6,12,44,32,4,8,12,17];
 var result = arr.reduce((sum, current) => sum + current);
 console.log(result);
@@ -10,4 +9,35 @@ let sum = strings.split(' ').map(str => parseInt(str)).reduce((sum, current) => 
 console.log(sum);
 
 // 3. Kirjuta funktsioon mis teisendab sekundid tundideks näiteks 7582 on 2 tundi.
+
+function toHours(sec) {
+  return "Päeva: " + Math.floor(sec / 86400)+ 
+    ", Tundi: " + Math.floor(sec / 3600) +
+    ", Minuti: " + Math.floor(sec % 3600 / 60) +
+    ", Sekundi: " +  Math.floor(sec % 3600 % 60);
+}
+console.log(toHours(86399));
+
+
 // 4. https://www.reddit.com/r/dailyprogrammer/comments/cdieag/20190715_challenge_379_easy_progressive_taxation/?utm_source=BD&utm_medium=Search&utm_name=Bing&utm_content=PSR1
+
+const  isString2 = (str1, string2) => {
+  const str1Doubled = str1 + str1;
+  if (Adoubled.includes(str2)) {
+  return true;
+    } else {
+  return false;
+    }
+  }
+  
+  console.log(isString2("nicole", "icolen"));
+  console.log(isString2("nicole", "lenico"));
+  console.log(isString2("nicole", "coneli"));
+  console.log(isString2("aabaaaaabaab", "aabaabaabaaa"));
+  console.log(isString2("abc", "cba"));
+  console.log(isString2("xxyyy", "xxxyy"));
+  console.log(isString2("xyxxz", "xxyxz")) ;
+  console.log(isString2("x", "x"));
+  console.log(isString2("x", "xx")) ;
+  console.log(isString2("x", ""));
+  console.log(isString2("", ""));
