ac5f4e0e374f85b0a71ecf9923c66ffcf1597011
Katrin Porkul <86647070+Porkul@users.noreply.github.com>
2021-10-29 15:41:51 +0300

    Create array.js

---
+++ homework/array.js
@@ -0,0 +1,4 @@
+// 1. Võta array [2,7,6,12,44,32,4,8,12,17] ja leidke seal olevate arvude kogusumma
+// 2. Võta string "2 7 6 12 44 32 4 8 12 17" ja leidke seal olevate arvude kogusumma, iga arv on eraldatud tühikuga
+// 3. Kirjuta funktsioon mis teisendab sekundid tundideks näiteks 7582 on 2 tundi.
+// 4. https://www.reddit.com/r/dailyprogrammer/comments/cdieag/20190715_challenge_379_easy_progressive_taxation/?utm_source=BD&utm_medium=Search&utm_name=Bing&utm_content=PSR1
