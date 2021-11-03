4d19aa6b7193da637351029ccecb750220047667
Katrin Porkul <86647070+Porkul@users.noreply.github.com>
2021-11-03 14:31:02 +0200

    Create starSquares.js

--- homework/starSquares.js
+++ homework/starSquares.js
@@ -5,12 +5,39 @@ rect(7,4)
 *******
 *******
 *******
+*/
+
+var horizCount = 7;
+var vertCount = 4;
+var str = '*'.repeat(horizCount);
+console.log(str);
+
+for (var i=1; i < vertCount; ++i) {
+  console.log(str);;
+}
+
+/*
 Kirjuta funktsioon mis prindib konsooli tühja ristküliku vastavalt mõõtmetele.
 hollowRect(7,4)
 *******
 *     *
 *     *
 *******
+*/
+
+let horizCount = 7;
+let vertCount = 4;
+let str = '*'.repeat(horizCount);
+
+console.log(str);
+
+for (let i=0; i < vertCount - 2; ++i) {
+  console.log('*' + (' '.repeat(horizCount - 2)) + '*');
+}
+
+console.log(str);
+
+/*
 Kirjuta funktsioon mis prindib konsooli ristküliku mille sees on rist/diagonaalid vastavalt mõõtmetele.
 diagonalRect(9,9)
 *********
@@ -22,6 +49,9 @@ diagonalRect(9,9)
 * *   * *
 **     **
 *********
+*/
+
+/*
 Väiksemate ja paaris arvuliste korral tulgu enam vähem nii nagu tuleb
 diagonalRect(7,5)
 *******
