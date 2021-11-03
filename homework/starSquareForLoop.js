31191607c47461744cd49f3e7953785ee9f913e4
Katrin Porkul <86647070+Porkul@users.noreply.github.com>
2021-11-03 13:28:07 +0200

    Create starSquareForLoop.js

---
+++ homework/starSquareForLoop.js
@@ -0,0 +1,38 @@
+/*
+Kirjuta funktsioon mis prindib konsooli ristküliku vastavalt mõõtmetele.
+rect(7,4)
+*******
+*******
+*******
+*******
+Kirjuta funktsioon mis prindib konsooli tühja ristküliku vastavalt mõõtmetele.
+hollowRect(7,4)
+*******
+*     *
+*     *
+*******
+Kirjuta funktsioon mis prindib konsooli ristküliku mille sees on rist/diagonaalid vastavalt mõõtmetele.
+diagonalRect(9,9)
+*********
+**     **
+* *   * *
+*  * *  *
+*   *   *
+*  * *  *
+* *   * *
+**     **
+*********
+Väiksemate ja paaris arvuliste korral tulgu enam vähem nii nagu tuleb
+diagonalRect(7,5)
+*******
+**   **
+*  *  *
+**   **
+*******
+või
+*******
+* * * *
+*  *  *
+* * * *
+*******
+*/
