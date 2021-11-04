37bc98f19a5402cb985a66859915a8f2d48c84b8
Katrin Porkul <86647070+Porkul@users.noreply.github.com>
2021-11-04 17:49:55 +0200

    Create balloonAttack.js

---
+++ practice/balloonAttack.js
@@ -0,0 +1,45 @@
+*/
+class Player {
+  constructor(name, hitsPerMinute) {
+    this.name = name;
+    this.hitsPerMinute = hitsPerMinute;
+    this.balloonCount = 100;
+  }
+ 
+  status() {
+    console.log(`Player: ${this.name} -- Balllons Left: ${this.balloonCount}`)
+  }
+}
+
+Write a game function balloonAttack that takes two Player instances, calculates the balloons left for each player after 10 minutes 
+(using the hitsPerMinute property) and returns the name of the winner.
+If the result is a tie, return the string 'Tie'.
+*/
+
+class Player {
+  constructor(name, hitsPerMinute) {
+    this.name = name;
+    this.hitsPerMinute = hitsPerMinute;
+    this.balloonCount = 100;
+  }
+
+  status() {
+    console.log(`Player: ${this.name} -- Balllons Left: ${this.balloonCount}`)
+  }
+}
+
+// Write function below
+const balloonAttack = (player1, player2) => {
+  for (let i = 1; i <= 10; i++){
+    player2.balloonCount -= player1.hitsPerMinute
+    player1.balloonCount -= player2.hitsPerMinute
+    player1.status();
+    player2.status();
+  }
+  if (player1.balloonCount > player2.balloonCount) {
+    return player1.name;
+  }else if (player2.balloonCount > player1.balloonCount)
+    return player2.name
+
+  return 'Tie'
+}
